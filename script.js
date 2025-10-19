c46bc362b6c88cb4dfd6fad2ec3af0acf11e1942
jjiji16 <djiyun16@gmail.com>
2025-10-20 01:28:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 🚨 [필수 교체] 1. Google Apps Script 배포 시 얻은 Web App URL로 교체하세요.
-const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzj4KJBdbGQqpxB9QvSAm9A1JYBpYAiIauNhLU_axj0gZPGLRJOzfIPZA35JudkYjU7/exec'; 
+const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzVRiy6P1IacWEMYnXSUdiGWS2XXn2Hj9kFLpiK7IspEc2ru7so5TNzM0g7Pb3PhSRH/exec'; 
 
 // 🚨 [필수 교체] 2. Google Sheets에서 차트 게시 후 얻은 iframe src 속성 값으로 교체하세요.
 const CHART_EMBED_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRuD1zwZn-7ezRlEQdw_UHa1wlMeuGi6-IjQOhEmGVbZMFulx1dnWAmdnJ26b2xRSG5MS1LDKL25Q0R/pubchart?oid=931016829&amp;format=image'; 
